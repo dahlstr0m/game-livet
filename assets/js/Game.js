027a31e9e6a53116f7349e9ab2c74f367f7223f6
Khiem Duc Tran <khiemdt@stud.ntnu.no>
2019-11-03 15:39:26 +0100

    Update Game.js

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -1,17 +1,15 @@
 // Initialiser spillet, angi størrelse på spillvindu.
 Crafty.init(1000,700, document.getElementById('game'));
 
+// Boundaries for brettet.
+Crafty.map.boundaries({min: {x:0, y:0}, max: {x:1000, y:700}})
+
 // Fast bakgrunn.
 Crafty.background("#7DDEEF url(assets/img/sky1.png) no-repeat center center");
 
 // Bakken som spilleren løper på.
 Crafty.e('Floor, 2D, Canvas, Color')
   .attr({x: 0, y: 600, w: 1000, h: 100})
-  .color('#303030');
-
-// Left border - utenfor spillet for å hindre at spilleren løper utenfor.
-Crafty.e('2D, Canvas, Solid, Color')
-  .attr({x: -100, y: 0, w: 100, h: 700})
   .color('#303030');
 
 // Legg til spiller.
