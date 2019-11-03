450ad851b8a1045363be53592420c31f73977873
Khiem Duc Tran <khiemdt@stud.ntnu.no>
2019-11-03 13:55:53 +0100

    Bakgrunnsbilde OK

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -1,2 +1,2 @@
 Crafty.init(1000,700, document.getElementById('game'));
-Crafty.background("#7DDEEF url(../img/sky1.png) no-repeat center center");
+Crafty.background("#7DDEEF url(assets/img/sky1.png) no-repeat center center");
