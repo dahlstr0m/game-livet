0937131ac6e0663244451d4a1fc08be5560c0aef
Khiem Duc Tran <khiemdt@stud.ntnu.no>
2019-11-03 14:19:46 +0100

    Lagt til spiller og gulv - ikke gå over kanten!

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -1,2 +1,23 @@
+// Initialiser spillet, angi størrelse på spillvindu.
 Crafty.init(1000,700, document.getElementById('game'));
+
+// Fast bakgrunn.
 Crafty.background("#7DDEEF url(assets/img/sky1.png) no-repeat center center");
+
+// Bakken som spilleren løper på.
+Crafty.e('Floor, 2D, Canvas, Color')
+  .attr({x: 0, y: 600, w: 1000, h: 100})
+  .color('#303030');
+
+// Left border - utenfor spillet for å hindre at spilleren løper utenfor.
+Crafty.e('2D, Canvas, Solid, Color')
+  .attr({x: -100, y: 0, w: 100, h: 700})
+  .color('#303030');
+
+// Legg til spiller.
+Crafty.e('2D, Canvas, Color, Twoway, Gravity')
+  .attr({x: 0, y: 0, w: 25, h: 50})
+  .stopOnSolids()
+  .color('#F00')
+  .twoway(200)
+  .gravity('Floor');
