714af0a96d0d825b66072870c4ad0d1ae012e913
mdahlstrooom <mathias.dahlstroem@gmail.com>
2019-11-03 17:30:51 +0100

    Lagt inn V1 av random fiender

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -12,9 +12,9 @@ Crafty.e('Floor, 2D, Canvas, Color')
   .attr({x: 0, y: 600, w: 1000, h: 100})
   .color('#303030');
 
-// Legg til spiller.
+// Spiller.
 Crafty.e('2D, Canvas, Color, Twoway, Gravity, Collision, spiller')
-  .attr({x: 200, y: 0, w: 100, h: 200})
+  .attr({x: 200, y: 0, w: 75, h: 125})
   .color('#F00')
   .twoway(200)
   .gravity('Floor')
@@ -23,7 +23,7 @@ Crafty.e('2D, Canvas, Color, Twoway, Gravity, Collision, spiller')
     this.x=0;
   })
   .onHit("VeggMidt", function(){
-    this.x=500;
+    this.x=526;
   })
   .bind("HitOn", function(hitData) {
     Crafty("Vegg").color('red');
@@ -51,6 +51,27 @@ Crafty.e("2D, Canvas, Color, Vegg")
       h: 700
     })
     .color('black');
+// FiendtligObjekt
+setInterval(spawnFiendlig, 1000);
+function spawnFiendlig(){
+  Let randomY = Math.floor((Math.random()*700) -20);
+    Crafty.e("2D, Canvas, Color, FiendtligObjekt")
+      .attr({
+        x: 1050,
+        y: randomY,
+        w: 40,
+        h: 40,
+        hSpeed: -2,
+        rotation: 45
+      })
+      .color('orange')
+      .bind('EnterFrame', function() {
+        this.x += this.hSpeed;
+      })
+};
+
+
+
 
 //Generer bakgrunnstall til utviklingsøyemed
 let bgData = document.getElementById('game');
@@ -62,5 +83,5 @@ bgData.appendChild(p);
 
 setInterval(bgDataOppdater, 100);
 function bgDataOppdater() {
- document.getElementById("bgData").innerText = "X: "+ Crafty("spiller").x.toFixed(1) + ' , ' + "Y: " + Crafty("spiller").y.toFixed(1);
+ document.getElementById("bgData").innerText = "_  X: "+ Crafty("spiller").x.toFixed(1) + ' , ' + "Y: " + Crafty("spiller").y.toFixed(1) + "Random y= " + randomY;
 }
